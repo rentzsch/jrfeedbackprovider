b07d8a643a68955666008b66e034a051f2b42ac4
rentzsch <jwr.git@redshed.net>
2009-02-03 19:01:53 -0600

    [NEW] Set the user's reply address in the server-side PHP script. (Michael Zornek)

--- jrfeedback.php
+++ jrfeedback.php
@@ -13,17 +13,32 @@ if (array_key_exists('feedback', $_REQUEST)) {
 	$feedbackType = $_REQUEST['feedbackType'];
 	$appName = $_REQUEST['appName'];
 	
-	$email = $_REQUEST['email'];
+	// if they choose not to give us their email, I will use my own
+	// if I left the email blank it would come from 
+	// suppressed@auma.pair.com or anonymous@auma.pair.com
+	// FogBugz would try to send them an email and generate another 
+	// ticket telling me "Undelivered Mail Returned to Sender"
+	// TODO: It'd be nice to make sure the string looks like a 
+	// real email address but I'll assume everyone is playing nice.
+	if ($_REQUEST['email'] == '') {
+	    $email = $_REQUEST['email'];
+	} else {
+        $email = 'YOUR_EMAIL_ADDRESS_HERE@gmail.com';
+    }
 	$feedback = $_REQUEST['feedback'];
 	$bundleID = $_REQUEST['bundleID'];
 	$systemProfile = $_REQUEST['systemProfile'];
+	
+	$headers = 'From: ' . $email . "\r\n" .
+        'Reply-To: ' . $email . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
 	
 	$msg .= "$feedback\n";
 	$msg .= "\n--------\n\n";
 	$msg .= "Bundle ID: $bundleID\n";
 	$msg .= "System Profile: $systemProfile\n";
 	
-	mail("YOUR_FEEDBACK_EMAIL_ADDRESS_HERE@gmail.com", "[$feedbackType] $appName", $msg);
+	mail("YOUR_FEEDBACK_EMAIL_ADDRESS_HERE@gmail.com", "[$feedbackType] $appName", $msg, $headers);
 }
 ?>
 
